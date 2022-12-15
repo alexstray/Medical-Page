b58a2ee66c533b3751ff5ab3f36042f9f2ee16d5
Alex Simpson <alexkiwilson@gmail.com>
2022-12-15 11:07:46 -0500

    12.15.2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['../../css/styles.min.css']
 })
 export class AppComponent {
   title = 'Medical-Page';
