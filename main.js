62b538035340c71079f72c7b1737192a21ceccad
Alex Simpson <alexkiwilson@gmail.com>
2022-12-19 13:15:26 -0500

    12.19.2022
    Complete Project

---
+++ main.js
@@ -0,0 +1,57 @@
+const navItems = document.querySelector('#nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+
+openNavBtn.addEventListener('click', () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+})
+
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+closeNavBtn.addEventListener('click', closeNav);
+
+
+
+//CLOSE NAV WHEN ITEM CLICKED
+if(window.innerWidth < 1024) {
+    document.querySelectorAll('#nav__items li a').forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            closeNav();
+        })
+    })
+}
+
+
+//CHANGE NAVBAR WHEN SCROLL
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window-scrollY > 0);
+})
+
+
+
+//TESTIMONIALS SWIPER//
+var swiper = new Swiper('.mySwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    //Responsive breakpoints
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
